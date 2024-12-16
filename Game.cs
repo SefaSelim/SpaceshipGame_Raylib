bf4e81df352dc1d4a83b80466eda9710e00cad75
SefaSelim <sefaselimtura@gmail.com>
2024-12-17 02:26:47 +0300

    Initializing the class structure

---
+++ Game.cs
@@ -0,0 +1,51 @@
+﻿using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceshipGame
+{
+    public class Game
+    {
+        public void SpaceShip()
+        {
+            Rectangle spaceship = new Rectangle();
+            spaceship.X = 0;
+            spaceship.Y = 0;
+            spaceship.Width = 50;
+            spaceship.Height = 50;
+            
+        }
+
+        public void StartGame() {
+
+            Raylib.InitWindow(800, 480, "Hello World");
+
+
+        }
+
+        public void UpdateGame()
+        {
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.White);
+
+                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+                Raylib.EndDrawing();
+            }
+        }
+
+
+
+
+        public void EndGame()
+        {
+            Raylib.CloseWindow();
+        }
+
+    }
+}
