100c2d2616b11e5f575df5d905b265d0a8fc2972
SefaSelim <sefaselimtura@gmail.com>
2024-12-18 02:50:40 +0300

    Added a moveable box for testing and control bindings // Created a class named Spaceship, created a method named control int Spaceship class for move functionalities // Created a class named Screen for attach all the screen elements from that class and intended to gain a mobility to the code

--- Game.cs
+++ Game.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,62 @@ namespace SpaceshipGame
 {
     public class Game
     {
-        public void SpaceShip()
+
+        public static class Screen
         {
-            Rectangle spaceship = new Rectangle();
-            spaceship.X = 0;
-            spaceship.Y = 0;
-            spaceship.Width = 50;
-            spaceship.Height = 50;
-            
+            public const int Height = 480;
+            public const int Width = 800;
+            public const string Title = "Spaceship Game";
+
         }
+
+
+        public class Spaceship
+        {
+            public static float Speed = 1f;
+            public static Vector2 Size = new Vector2(40f, 40f);
+            public static Vector2 Positions = new Vector2(10f, (Screen.Height - Size.X) / 2);
+            public static Rectangle Collision = new Rectangle(Positions,Size);
+
+            public static void control()
+            {
+                //çapraz yönlerde hız artışı problemi çözülecek
+
+                if (Raylib.IsKeyDown(KeyboardKey.A))
+                {
+                    Positions.X -= Positions.X <= 0 ? 0 : Speed / 10;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.D))
+                {
+                    Positions.X += Positions.X >= (Screen.Width - Size.X )? 0 : Speed / 10;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.W))
+                {
+                    Positions.Y -= Positions.Y <= 0 ? 0 : Speed / 10;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.S))
+                {
+                    Positions.Y += Positions.Y >= (Screen.Height - Size.Y) ? 0 : Speed / 10;
+                }
+
+                Collision = new Rectangle(Positions,Size);
+
+            }
+
+
+        }
+
+
+
+
+        //list enemies
+
+        public bool isGameOver = false;
+
+
+
+
+
 
         public void StartGame() {
 
@@ -33,7 +81,12 @@ namespace SpaceshipGame
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.White);
 
-                Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+                Spaceship.control();
+
+
+
+                //   Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+                Raylib.DrawRectangleV(Spaceship.Positions, Spaceship.Size, Color.Red);
 
                 Raylib.EndDrawing();
             }
